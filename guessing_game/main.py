a3d2a3f30b5a6372ef4a55fa03ac9b012eb52b8a
delta-roman <romanbista469@gmail.com>
2026-01-19 20:48:40 +0545

    new one

---
+++ guessing_game/main.py
@@ -0,0 +1,38 @@
+import random
+easy_words=["apple","train","money","nepal"]
+medium_words=["python","bottle","monkey","laptop"]
+hard_words=["elephant","diamond","computer","mountain"]
+
+print("Welcome to the password guessing game.")
+print("choose a difficulty level: easy,medium and hard")
+level=input("Enter difficulty level: ").lower()
+if level=="easy":
+    secret=random.choice(easy_words)
+elif level=="medium":
+    secret=random.choice(medium_words)
+elif level=="hard":
+    secret=random.choice(hard_words)
+else:
+    print("Invalid choice.defaulting to easy level")
+    secret=random.choice(easy_words)
+
+attempts=0
+print("\n Guess the secret password")
+
+while True:
+    guess= input("Enter your guesss: ").lower()
+    attempts+=1
+
+    if guess==secret:
+        print(f"congratulations! you guessed in it{attempts} attempts")
+        break
+
+    hint=""
+
+    for i in range(len(secret)):
+        if i <len(guess) and guess[i]== secret[i]:
+            hint+=guess[i]
+        else:
+            hint+="_"
+    print("Hint:",hint)
+print("GAME OVER")
